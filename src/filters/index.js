b469666d76488946b911e3fbcc3bd40262c2a594
yanglil8 <1653717386@qq.com>
2022-12-13 20:07:56 +0800

    firs

--- src/filters/index.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-
-Vue.filter('transformVisitCount',function(value) {
-  if(value >= 10000){
-    value = value/10000
-  }
-})
--- src/plugins/moment.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import vueMoment from 'vue-moment'
-import moment from 'moment'
-import 'moment/locale/zh-cn'
-
-
-Vue.use(vueMoment,{moment})
